4dd87b00dd1d29b8b0d09de33be19a532315783d
hoangnhancs <thaihoangnhantk17lqd@gmail.com>
2020-12-24 17:47:50 +0700

    upload projectCSharp and song_img_folder

---
+++ ProjectCSharp/ProjectCSharp/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ProjectCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnnext = new System.Windows.Forms.Button();
+            this.btnprev = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.btnnext);
+            this.panel1.Controls.Add(this.btnprev);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1503, 691);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1081, 149);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(339, 114);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Enabled = false;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(1212, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 84);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnnext
+            // 
+            this.btnnext.Location = new System.Drawing.Point(989, 325);
+            this.btnnext.Name = "btnnext";
+            this.btnnext.Size = new System.Drawing.Size(75, 23);
+            this.btnnext.TabIndex = 3;
+            this.btnnext.Text = ">";
+            this.btnnext.UseVisualStyleBackColor = true;
+            this.btnnext.Click += new System.EventHandler(this.btnnext_Click);
+            // 
+            // btnprev
+            // 
+            this.btnprev.Location = new System.Drawing.Point(48, 325);
+            this.btnprev.Name = "btnprev";
+            this.btnprev.Size = new System.Drawing.Size(75, 23);
+            this.btnprev.TabIndex = 2;
+            this.btnprev.Text = "<";
+            this.btnprev.UseVisualStyleBackColor = true;
+            this.btnprev.Click += new System.EventHandler(this.btnprev_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(88, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(943, 597);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1081, 326);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(339, 199);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1505, 694);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnnext;
+        private System.Windows.Forms.Button btnprev;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ ProjectCSharp/ProjectCSharp/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace ProjectCSharp
+{
+    public partial class Form1 : Form
+    {
+        string Dirpath;
+
+        int imgindex;
+        int imgindexprev;
+        int imgindexnext;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+
+
+
+            listBox1.Items.Clear();
+
+            Dirpath = @"E:\CSharp\song_img_folder";
+
+            string[] files = Directory.GetFiles(Dirpath, "*.Jpg");
+
+            foreach (string file in files)
+
+            {
+
+                int pos = file.LastIndexOf("||");
+
+                string FName = file.Substring(pos + 1);
+
+                listBox1.Items.Add(FName);
+
+            }
+
+            //listBox1.SelectedIndex = imgindex = 0;
+
+            btnprev.Enabled = true;
+
+            btnnext.Enabled = true;
+            imgindex = 0;
+            imgindexnext = 1;
+
+            imgindexprev = listBox1.Items.Count - 1;
+            pictureBox1.ImageLocation = Convert.ToString(listBox1.Items[imgindex]); ;
+            pictureBox2.ImageLocation = Convert.ToString(listBox1.Items[imgindexprev]);
+            pictureBox3.ImageLocation = Convert.ToString(listBox1.Items[imgindexnext]);
+            timer1.Interval = 4 * 1000;
+
+            timer1.Start();
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+
+            //DialogResult dr = folderBrowserDialog1.ShowDialog();
+
+            //if (dr != DialogResult.Cancel)
+
+            //{
+
+                
+
+            //}
+
+        }
+
+ 
+
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        {
+
+            pictureBox1.ImageLocation = Convert.ToString(listBox1.Items[imgindex]); ;
+            pictureBox2.ImageLocation = Convert.ToString(listBox1.Items[imgindexprev]);
+            pictureBox3.ImageLocation = Convert.ToString(listBox1.Items[imgindexnext]);
+            // pictureBox1.ImageLocation = Dirpath + "\\" + listBox1.SelectedItem;
+
+        }
+
+
+
+        private void btnprev_Click(object sender, EventArgs e)
+
+        {
+            /*
+            if (imgindex > 0)
+
+            {
+
+                imgindex -= 1;
+                
+                if (imgindex == 0)
+
+                {
+
+                    btnprev.Enabled = false;
+
+                }
+
+                if (imgindex < listBox1.Items.Count - 1)
+
+                    btnnext.Enabled = true;
+                
+
+                listBox1.SelectedIndex = imgindex;
+
+            }
+            else
+            {
+                imgindex = listBox1.Items.Count - 1;
+                listBox1.SelectedIndex = imgindex;
+            }
+            */
+            imgindexnext = imgindex;
+            imgindex = imgindexprev;
+            
+            imgindexprev -= 1;
+            if (imgindexprev < 0)
+            {
+                imgindexprev = listBox1.Items.Count - 1;
+            }
+            pictureBox1.ImageLocation = Convert.ToString(listBox1.Items[imgindex]); ;
+            pictureBox2.ImageLocation = Convert.ToString(listBox1.Items[imgindexprev]);
+            pictureBox3.ImageLocation = Convert.ToString(listBox1.Items[imgindexnext]);
+            timer1.Stop();
+            timer1.Start();
+        }
+
+
+
+        private void btnnext_Click(object sender, EventArgs e)
+
+        {
+            /*
+            if (imgindex < listBox1.Items.Count - 1)
+
+            {               
+                imgindex += 1;
+
+                //if (imgindex == listBox1.Items.Count - 1)
+
+                    //btnnext.Enabled = false;
+
+                //if (imgindex > 0)
+
+                    //btnprev.Enabled = true;
+
+                listBox1.SelectedIndex = imgindex;
+
+            }
+            else
+            {
+                imgindex = 0;
+                listBox1.SelectedIndex = imgindex;
+            }
+            */
+            imgindexprev = imgindex;
+            imgindex += 1;
+            imgindexnext = imgindex + 1;
+            
+            if (imgindex > listBox1.Items.Count -1)
+            {
+                imgindex = imgindex - listBox1.Items.Count;
+            }
+            if (imgindexnext > listBox1.Items.Count -1)
+            {
+                imgindexnext = imgindexnext - listBox1.Items.Count;
+            }
+            //listBox1.SelectedIndex = imgindex;
+            Console.WriteLine(imgindex);
+            Console.WriteLine(imgindexnext);
+            Console.WriteLine(imgindexprev);
+            pictureBox1.ImageLocation = Convert.ToString(listBox1.Items[imgindex]); ;
+            pictureBox2.ImageLocation = Convert.ToString(listBox1.Items[imgindexprev]);
+            pictureBox3.ImageLocation = Convert.ToString(listBox1.Items[imgindexnext]);
+            timer1.Stop();
+            timer1.Start();
+        }
+
+
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //pictureBox2.Image = pictureBox1.Image;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            btnnext.PerformClick();
+        }
+    }
+}
